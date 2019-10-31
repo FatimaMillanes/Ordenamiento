e0a7a50266c3ad4b06ec8a1627b6ec437bc9c387
fatim <fatima.rosela@hotmail.com>
2019-10-31 07:56:13 -0700

    Agregar archivos de proyecto.

---
+++ Ordenamiento/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace Ordenamiento
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<int> miLista = new ObservableCollection<int>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            miLista.Add(10);
+            miLista.Add(20);
+            miLista.Add(30);
+            miLista.Add(40);
+            miLista.Add(50);
+            miLista.Add(60);
+            miLista.Add(70);
+            miLista.Add(80);
+            miLista.Add(90);
+            miLista.Add(100);
+            lstNumeros.ItemsSource = miLista;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = miLista[0];
+            miLista[0] = miLista[3];
+            miLista[3] = temp;
+        }
+    }
+}
